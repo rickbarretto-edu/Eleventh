f744820e6fd8d3e16123ea092e50a151b504ba40
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-17 13:01:27 -0300

    Organize chat into smaller functions

--- test/integration/chat_tcp/server.py
+++ test/integration/chat_tcp/server.py
@@ -6,21 +6,31 @@ Chat = Connection
 
 clients: set[Chat] = set()
 
+def disconnect(chat):
+    clients.remove(chat)
+    print(f"[-] {chat.address} disconnected")
+
+def connect(chat):
+    clients.add(chat)
+    print(f"[+] {chat.address} connected")
+
+async def broadcast_others(self: Chat, message: str):
+    for client in clients:
+        if client is not self:
+            await client.send(f"{self.address}: {message}")
+
+
 async def chat(current: Chat) -> None:
     async with current:
-        clients.add(current)
-        print(f"[+] {current.address} connected")
+        connect(current)
         try:
             while True:
                 if not (msg := await current.receive()):
                     break
-                # Broadcast to others
-                for c in clients:
-                    if c is not current:
-                        await c.send(f"{current.address}: {msg}")
+                await broadcast_others(current, msg)
         finally:
-            clients.remove(current)
-            print(f"[-] {current.address} disconnected")
+            disconnect(current)
+
 
 async def main():
     async with Server.local_at(port=5000).handles(chat) as server:
