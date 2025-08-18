c874667b66a94c7020781d9159730c88fb4a6d35
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-17 22:14:13 -0300

    Document Protocol with Example

--- quickapi/rtp/__init__.py
+++ quickapi/rtp/__init__.py
@@ -1,2 +1,31 @@
 """RTP - Rick Transfer Protocol
+
+Message Exchange Example
+========================
+
+Request
+-------
+
+    POST /protocols/create RTP
+    size: ...
+    type: JSON
+
+    {
+        "protocol": "Rick Transfer Protocol",
+        "abbr": "RTP",
+    }
+
+    
+Response
+--------
+
+    RTP 200 OK
+    size: ...
+    type: HTML
+
+    <section>
+        Created Protocol: <strong>Rick Transfer Protocol (RTP)</strong>
+    </section>
+
+
 """
