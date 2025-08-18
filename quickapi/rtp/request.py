e46996ab1432ac79743352d3c2e3fef0385b3f0c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-18 18:26:48 -0300

    Implement hash for Method

--- quickapi/rtp/request.py
+++ quickapi/rtp/request.py
@@ -1,31 +1,7 @@
 from __future__ import annotations
-import enum
-from typing import Final, Self
+from typing import Final
 
 import attrs
-
-
-@attrs.frozen
-class Request:
-    method: Method
-    path: Path
-    body: Body
-
-    def __str__(self) -> str:
-        return "\n".join([
-            "{method} {path} {version}",
-            "size: {size}",
-            "type: {type}"
-            "",
-            "{body}"
-        ]).format(
-            version="RTP/1.0",
-            method=self.method,
-            path=self.path,
-            size=len(self.body),
-            type=self.body.type,
-            body=self.body,
-        )
 
 @attrs.frozen
 class Path:
@@ -41,11 +17,23 @@ class _MethodMeta(type):
 
 
 class Method(metaclass=_MethodMeta):
+
     def __init__(self, value: str) -> None:
         self.value: Final[str] = value.upper()
 
     def __str__(self) -> str:
         return self.value
+    
+    def __repr__(self) -> str:
+        return f"Method({self.value!r})"
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Method):
+            return self.value == other.value
+        return NotImplemented
+
+    def __hash__(self) -> int:
+        return hash(self.value)
 
 @attrs.frozen
 class Body:
@@ -77,3 +65,26 @@ class PlainText(Body):
 class Xml(Body):
     def __init__(self, content: str):
         super().__init__(content, "xml")
+
+
+@attrs.frozen
+class Request:
+    method: Method
+    path: Path
+    body: Body = Body("", "")
+
+    def __str__(self) -> str:
+        return "\n".join([
+            "{method} {path} {version}",
+            "size: {size}",
+            "type: {type}"
+            "",
+            "{body}"
+        ]).format(
+            version="RTP/1.0",
+            method=self.method,
+            path=self.path,
+            size=len(self.body),
+            type=self.body.type,
+            body=self.body,
+        )
