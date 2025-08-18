42e0bb7017bdbd04eabc684dd99733af5e3a6fdb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-17 22:21:53 -0300

    Include version on documentation

--- quickapi/rtp/__init__.py
+++ quickapi/rtp/__init__.py
@@ -6,12 +6,13 @@ Message Exchange Example
 Request
 -------
 
-    POST /protocols/create RTP
+    POST /protocols/create RTP/1.0
     size: ...
     type: JSON
 
     {
         "protocol": "Rick Transfer Protocol",
+        "version": "1.0",
         "abbr": "RTP",
     }
 
@@ -19,12 +20,12 @@ Request
 Response
 --------
 
-    RTP 200 OK
+    RTP/1.0 200 OK
     size: ...
     type: HTML
 
     <section>
-        Created Protocol: <strong>Rick Transfer Protocol (RTP)</strong>
+        Created Protocol: <strong>Rick Transfer Protocol v1.0 (RTP/1.0)</strong>
     </section>
 
 
