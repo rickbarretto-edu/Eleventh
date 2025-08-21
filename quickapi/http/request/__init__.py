dfc4aba7d0e6a42bb4eac04e067a843cd7796302
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-21 12:58:46 -0300

    Finish Request/Response

---
+++ quickapi/http/request/__init__.py
@@ -0,0 +1,47 @@
+import attrs
+
+from quickapi.http.request.method import Method
+from quickapi.http.request.target import Target
+from quickapi.http.version import Version
+from quickapi.http.body import Body
+
+__all__ = [
+    "Method",
+    "Request",
+    "Target",
+]
+
+
+@attrs.frozen
+class Request:
+    method: Method
+    path: Target
+    body: Body = Body.empty()
+    keep_alive: bool = False
+
+    @property
+    def version(self) -> Version:
+        return Version("1.0")
+
+    def __str__(self) -> str:
+        headers: list[str] = [
+            "{method} {path} RTP/{version}",
+            "Content-Length: {size}",
+        ]
+
+        if len(self.body) > 0:
+            headers.append("Content-Type: {type}")
+
+        headers.append("Connection: {connection}")
+        headers.append("")
+        headers.append("{body}")
+
+        return "\n".join(headers).format(
+            method=self.method,
+            path=self.path,
+            version=self.version,
+            size=len(self.body),
+            type=self.body.mime,
+            connection="keep-alive" if self.keep_alive else "close",
+            body=self.body,
+        )
---
+++ quickapi/http/response/__init__.py
@@ -0,0 +1,37 @@
+import attrs
+
+from quickapi.http.body import Body
+from quickapi.http.response.status import Status
+from quickapi.http.version import Version
+
+
+@attrs.frozen
+class Response:
+    status: Status
+    body: Body = Body.empty()
+    keep_alive: bool = False
+
+    version: Version = attrs.field(default=Version("1.1"), init=False)
+
+    def __str__(self) -> str:
+        headers: list[str] = [
+            "{version} {code} {reason}",
+            "Content-Length: {size}",
+        ]
+
+        if len(self.body) > 0:
+            headers.append("Content-Type: {type}")
+
+        headers.append("Connection: {connection}")
+        headers.append("")
+        headers.append("{body}")
+
+        return "\n".join(headers).format(
+            version=self.version,
+            code=self.status.code,
+            reason=self.status.reason,
+            size=len(self.body),
+            type=self.body.mime,
+            connection="keep-alive" if self.keep_alive else "close",
+            body=self.body,
+        )
