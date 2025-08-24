e87742e790404494387bcc88f45051bb32d62026
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-24 14:57:20 -0300

    Add type hints

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,24 +7,29 @@ use quickapi::response::Response;
 
 #[tokio::main]
 async fn main() {
-    let mut app = Server::new();
+    let mut app: Server = Server::new();
 
     // / => Welcome to Eleventh
-    app.route("/", |_req, _params| Response::new("Welcome to Eleventh!"));
+    app.route("/", |_req, _params| 
+        Response::new("Welcome to Eleventh!")
+    );
 
     // /greet?name="Rick" => Hello, Rick?
     app.route("/greet", |req, _params| {
-        let binding = "Anonymous".to_string();
-        let name = req.param("name").unwrap_or(&binding);
+        let binding: String = "Anonymous".to_string();
+        let name: &String = req.param("name").unwrap_or(&binding);
+        
         Response::new(&format!("Hello, {}!", name))
     });
 
     // /users/123 => {"user_id":"123"}
     app.route("/users/{id}", |_req, params: HashMap<String, String>| {
-        let id = params.get("id").unwrap();
+        let id: &String = params.get("id").unwrap();
+        
         Response::json(&json!({"user_id": id}))
     });
 
     println!("Starting Eleventh app on 127.0.0.1:8080");
     app.run("127.0.0.1:8080").await;
+
 }
