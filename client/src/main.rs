b74c1b965b9056892603dbae9baf67e3a79dce6e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-31 17:15:10 -0300

    Apply modern theme to it

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,10 +7,14 @@ use cursive::views::TextView;
 use cursive::Cursive;
 use cursive::*;
 
+use eleventh::theme;
+
 fn main() {
     let mut app = Cursive::default();
-
+    
     WelcomeScreen(&mut app);
+
+    app.set_theme(theme::theme());
     app.run();
 }
 
---
+++ client/src/theme.rs
@@ -0,0 +1,37 @@
+use cursive::theme::{BaseColor, Color, Palette, PaletteColor, BorderStyle, Theme};
+
+pub fn theme() -> Theme {
+    let mut palette = Palette::default();
+    
+    const WHITE: Color = Color::Dark(BaseColor::White);
+    const GRAY_150: Color = Color::Rgb(150, 150, 150);
+    const GRAY_180: Color = Color::Rgb(180, 180, 180);
+    const GLOBAL_BACKGROUND: Color = Color::Rgb(20, 20, 28);
+    const NEON_BLUE: Color = Color::Rgb(0, 150, 255);
+    const METALLIC_GOLD: Color = Color::Rgb(212, 175, 55);
+    const DARK_SHADOW: Color = Color::Rgb(10, 10, 15);
+
+    // Global
+    palette[PaletteColor::View] = GLOBAL_BACKGROUND;
+    palette[PaletteColor::Background] = GLOBAL_BACKGROUND;
+
+    // Primary text
+    palette[PaletteColor::Primary] = WHITE;
+    palette[PaletteColor::TitlePrimary] = WHITE;
+
+    // Secondary text
+    palette[PaletteColor::Secondary] = GRAY_150;
+    palette[PaletteColor::TitleSecondary] = GRAY_180;
+
+    palette[PaletteColor::Highlight] = NEON_BLUE;
+    palette[PaletteColor::HighlightText] = WHITE;    
+    palette[PaletteColor::HighlightInactive] = METALLIC_GOLD;
+
+    palette[PaletteColor::Shadow] = DARK_SHADOW;
+
+    Theme {
+        shadow: true,
+        borders: BorderStyle::Outset,
+        palette,
+    }
+}
