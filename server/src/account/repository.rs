7aa43b982297e204a2e426f0dabc0f79e8435fbf
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-05 23:07:23 -0300

    Remove Shared account

--- server/src/account/repository.rs
+++ server/src/account/repository.rs
@@ -1,29 +1,18 @@
 use std::collections::HashMap;
-use std::sync::Arc;
-use tokio::sync::Mutex;
 
 use super::models::Account;
 
-/// Wrapper around Accounts to avoid Arc::clone boilerplate
-pub struct SharedAccounts(Arc<Mutex<VirtualAccounts>>);
-
 #[derive(Clone, Debug)]
-pub struct VirtualAccounts {
+pub struct Accounts {
     map: HashMap<String, Account>,
 }
 
-impl VirtualAccounts {
+impl Accounts {
     pub fn new() -> Self {
-        VirtualAccounts {
-            map: HashMap::new(),
-        }
+        Accounts { map: HashMap::new() }
     }
 
-    pub fn shared(self) -> SharedAccounts {
-        SharedAccounts::new(self)
-    }
-
-    fn store_inner(&mut self, new_account: Account) -> Result<(), String> {
+    pub async fn store(&mut self, new_account: Account) -> Result<(), String> {
         if self.map.contains_key(&new_account.username) {
             return Err("Account already exists".into());
         }
@@ -31,31 +20,9 @@ impl VirtualAccounts {
         Ok(())
     }
 
-    fn by_credentials_inner(&self, username: &str, password: &str) -> Option<&Account> {
+    pub async fn by_credentials(&self, username: &str, password: &str) -> Option<&Account> {
         self.map
             .get(username)
             .filter(|acc| acc.password == password)
-    }
-}
-
-impl SharedAccounts {
-    pub fn new(repo: VirtualAccounts) -> Self {
-        SharedAccounts(Arc::new(Mutex::new(repo)))
-    }
-
-    pub async fn store(&self, new_account: Account) -> Result<(), String> {
-        let mut repo = self.0.lock().await;
-        repo.store_inner(new_account)
-    }
-
-    pub async fn by_credentials(&self, username: &str, password: &str) -> Option<Account> {
-        let repo = self.0.lock().await;
-        repo.by_credentials_inner(username, password).cloned()
-    }
-}
-
-impl Clone for SharedAccounts {
-    fn clone(&self) -> Self {
-        SharedAccounts(self.0.clone())
     }
 }
