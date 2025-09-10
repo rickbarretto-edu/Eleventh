ccba7036b9dba47316b29d8c45c508f1f248bbbb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-10 10:08:30 -0300

    Rename variables

--- client/src/screens/championship/pairing.rs
+++ client/src/screens/championship/pairing.rs
@@ -3,14 +3,10 @@ use cursive::views::Dialog;
 use std::time::{Duration, Instant};
 use std::thread;
 
+use super::NamePlayers;
+
 fn fetch(start: Instant) -> bool {
     start.elapsed() >= Duration::from_millis(3500)
-}
-
-#[allow(non_snake_case)]
-pub fn Match(app: &mut Cursive, auth: String) {
-    app.pop_layer();
-    app.add_layer(Dialog::info(format!("Matched with auth: {}", auth)));
 }
 
 #[allow(non_snake_case)]
@@ -24,16 +20,16 @@ pub fn Pairing(app: &mut Cursive, auth: String) {
     app.add_layer(view);
 
     let sink = app.cb_sink().clone();
-    let auth_clone = auth.clone();
+    let auth = auth.clone();
     let start = Instant::now();
 
     thread::spawn(move || {
         loop {
             thread::sleep(Duration::from_secs(1));
             if fetch(start) {
-                let auth_for_cb = auth_clone.clone();
+                let auth = auth.clone();
                 sink.send(Box::new(move |app: &mut Cursive| {
-                    Match(app, auth_for_cb.clone());
+                    NamePlayers(app, auth.clone());
                 })).unwrap();
                 break;
             }
