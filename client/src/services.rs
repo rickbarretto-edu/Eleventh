26c7317d1f8eb423a583bd4fb1686ebd0455195c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-12 17:23:29 -0300

    Create shared function

--- client/src/services.rs
+++ client/src/services.rs
@@ -1,3 +1,14 @@
 pub mod account;
 pub mod championship;
 pub mod deck;
+
+/// Returns the Eleventh's Server URL
+/// 
+/// This value may change according to the environment,
+/// this allows the compatibility for locally deployed environments
+/// which allow us to develop, prototype and build quickly, and also,
+/// for production, which is used along side Docker.
+pub fn server_url() -> String {
+    std::env::var("ELEVENTH_ADDRESS")
+        .unwrap_or("127.0.0.1:8080".into())
+}
