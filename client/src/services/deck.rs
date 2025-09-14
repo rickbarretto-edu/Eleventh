3e4c064b585b0317ec3e3a700e884e339dc9bd77
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-14 17:08:28 -0300

    FIx url
    The URL changes accoding to the environment

--- client/src/services/deck.rs
+++ client/src/services/deck.rs
@@ -1,5 +1,7 @@
+use crate::services::server_url;
+
 pub fn list(auth: &String) -> Result<reqwest::blocking::Response, reqwest::Error> {
     let client = reqwest::blocking::Client::new();
-    let url: String = format!("http://server:8080/user/{}/deck/", auth);
+    let url: String = format!("http://{}/user/{}/deck/", server_url(), auth);
     client.get(&url).send()
 }
