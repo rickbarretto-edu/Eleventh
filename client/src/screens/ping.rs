e6cb292a1862c5340ca7ccc9509cc9770f2c3c88
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-14 17:45:28 -0300

    Implement ping screen properly

--- client/src/screens/ping.rs
+++ client/src/screens/ping.rs
@@ -1,8 +1,23 @@
 use cursive::views::Dialog;
 use cursive::Cursive;
 
+use crate::services;
+
 #[allow(non_snake_case)]
 pub fn Ping(app: &mut Cursive) {
-    let view = Dialog::info("Ping feature is under construction.");
+    let message: String = match services::ping::ping(10) {
+        Ok(duration) => {
+            let ms = duration.as_millis();
+            if ms < 1000 {
+                format!("Ping successful: {} ms", ms)
+            } else {
+                let secs = duration.as_secs_f64();
+                format!("Ping successful: {:.2} s", secs)
+            }
+        }
+        Err(err) => format!("Ping failed: {}", err),
+    };
+    
+    let view = Dialog::info(message);
     app.add_layer(view);
 }
