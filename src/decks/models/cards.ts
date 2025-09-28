7e45d7b89e404e0f0658ed30a470f4baf25c8c50
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-09-28 19:47:07 -0300

    Purge old source code

--- src/decks/models/cards.ts
+++
@@ -1,8 +0,0 @@
-
-
-export type Card = {
-    id: string
-    name: string
-    position: 'atk' | 'mid' | 'def' | 'gk'
-    score: number
-}
--- src/decks/models/deck.ts
+++
@@ -1,7 +0,0 @@
-import { Card } from "./cards.ts";
-
-export type Deck = {
-    id: string
-    owner: string
-    cards: Card[]
-}
