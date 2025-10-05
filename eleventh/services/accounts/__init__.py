438d3790e0d074c826a72f4ca31ff873cd73be02
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-05 18:43:18 -0300

    Add manual entry

---
+++ eleventh/services/accounts/__init__.py
@@ -0,0 +1,9 @@
+"""Accounts service API.
+
+This module defines the API routes for the accounts service, allowing clients to create and authenticate user accounts.
+
+"""
+
+from eleventh.services.accounts.api import route
+
+__all__ = ["route"]
