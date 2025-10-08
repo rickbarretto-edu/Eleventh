323e7cedc6c22b140335d5dbf9bc8122467f9833
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-07 22:23:21 -0300

    Remove old structure

--- eleventh/services/accounts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Accounts service API.
-
-This module defines the API routes for the accounts service, allowing clients to create and authenticate user accounts.
-
-"""
-
-from eleventh.services.accounts.api import route
-
-__all__ = ["route"]
