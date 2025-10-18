6572c3c59257d0b6ce89cfff0909824c15868104
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-18 15:14:06 -0300

    Implement sketch of rewarding system

--- rewards/__main__.py
+++ rewards/__main__.py
@@ -1,22 +1,62 @@
-"""Rewarding System
+"""Rewarding Generation service.
 
-This subsystem generates the rewards used in Eleventh
-and does not store anything.
+At A Glance
+===========
 
-This service is used with lower frequency,
-and thus, there is no need for any complex algorithm.
+Run this using:
 
-Leader ellection happens via Bully algorithm, so the higest ID will be choosen. 
-Since this doesn't need data consistency, the follower just redirects the generated deck to the user.
+    $ python -m rewards --host 127.0.0.1 --port 8001
+    $ python -m rewards --host 127.0.0.1 --port 8002
+    $ python -m rewards --host 127.0.0.1 --port 8003
 
-The other followers will only work as a backup service,
-so when the highest server goes down, the next highest one takes control.
+
+Open https://127.0.0.1:8001/admin on Browser and connect to other peers.
+Then, open https://127.0.0.1:8001 and click "Generate" button to generate text and get rewards from other peers.
+
+
+Running from Shell
+==================
+
+After running the above commands, you can also interact with the service:
+
+Service
+~~~~~~~~~~
+
+Generate 500 Cards
+------------------
+
+    $ curl -X POST https://127.0.0.1:8001/api/create?n=500
+
+
+Management
+~~~~~~~~~~
+
+Attach Peer
+-----------
+
+    $ curl -X POST https://127.0.0.1:8001/cluster/attach -d '{"peer":"127.0.0.1:8002"}' -H "Content-Type: application/json"
+    $ curl -X POST https://127.0.0.1:8001/cluster/attach -d '{"peer":"127.0.0.1:8003"}' -H "Content-Type: application/json"
+
+Detach Peer
+-----------
+
+    $ curl -X POST https://127.0.0.1:8001/cluster/detach -d '{"peer":"127.0.0.1:8002"}' -H "Content-Type: application/json"
+
+Cluster Health
+--------------
+
+    $ curl https://127.0.0.1:8001/cluster/health
+    { "peers": [{"address": "127.0.0.1:8002", "status": "active"}, {"address": "127.0.0.1:8003", "status": "inactive"}] }
+
 """
 
-from fastapi.applications import FastAPI
+from __future__ import annotations
 
-from rewards.routes import router
+from fastapi import FastAPI
 
 
-app = FastAPI()
-app.include_router(router)
+app = FastAPI(
+    title="Rewarding Generation Service", 
+    version="0.1.0",
+    debug=True,
+)
--- rewards/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-router.include_router(api)
-router.include_router(cluster)
