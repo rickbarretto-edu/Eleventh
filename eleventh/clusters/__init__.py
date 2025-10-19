2fe03bbfcc0eebdc93eaff0319fc0f8f83a769eb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-19 20:19:27 -0300

    Restart from scratch

--- eleventh/clusters/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Clustering utilities for distributed applications."""
-
-from eleventh.clusters.plugin import ConsulPlugin, PeerStatus, ClusterInfo, HealthCheckResponse
-
-__all__ = ["ConsulPlugin", "PeerStatus", "ClusterInfo", "HealthCheckResponse"]
